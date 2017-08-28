8bb172ecba0d0dd9d4d6f3c1aa4452c132aeb6ca
Arun Sood <arsood@gmail.com>
2017-08-28 15:43:01 -0700

    Show data on edit form

--- index.js
+++ index.js
@@ -56,6 +56,26 @@ app.post("/wines", (req, res) => {
     });
 });
 
+app.get("/wines/:id/edit", (req, res) => {
+    //Step 1: Perform cURL GET request to API using the ID from the route parameter
+
+    //Step 2: Render edit.ejs with information coming back from the API
+
+    request({
+        method: "GET",
+        uri: "http://myapi-profstream.herokuapp.com/api/d3397e/wines/" + req.params.id,
+        json: true
+    })
+    .then((wineData) => {
+        res.render("edit", {
+            wine: wineData
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
 app.listen(3000, () => {
     console.log("Server started on port 3000...");
 });
